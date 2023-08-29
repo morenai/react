28d8cbcec72aa708fcd155e51a754d9144bd2120
morenai <miguel.a.castle@gmail.com>
2023-08-29 18:23:53 +0200

    all ok. pending create todo

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,25 +1,22 @@
+import { CompletedIcon } from './CompletedIcon';
+import { DeleteIcon } from './DeleteIcon';
 import './TodoItem.css';
 
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span 
-        className={`Icon Icon-check ${props.
-        completed && "Icon-check--active"}`}
-          onClick={props.onComplete}
-          >
-        V
-      </span>
-      <p className={`TodoItem-p ${props.
-        completed && "TodoItem-p--complete"}`}>
+      <CompletedIcon 
+        completed={props.completed}
+        onComplete={props.onComplete}
+        />
+
+        <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete"
-      onClick={props.onDelete}
-      >
-        X
-      </span>
-    </li>
+      <DeleteIcon 
+      onDelete={props.onDelete}
+      />
+      </li>
   );
 }
 
